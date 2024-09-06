b260bc4fc86cce17c3aef7ba165bbb9260f46336
Yashkumar patil <136915825+yashpatil209@users.noreply.github.com>
2024-09-06 22:11:16 +0530

    first commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD;
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
     serverSelectionTimeoutMS: 5000, 
 });
 
